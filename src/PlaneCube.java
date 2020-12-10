ea90def87115f04a6cd50be449ece4ab53e5e2c2
kimzn12 <kimzn12@naver.com>
2020-12-10 21:56:11 +0900

    [add] PlaneCube
    * run: 리펙토링
    * TurnLeft: 큐브 왼쪽,위쪽으로 돌리기
    * TurnRight: 큐브 오른쪽, 아래쪽으로 돌리기
    * ArrayToString: 라인(U,R,L,B)의 배열을 문자열로 합치기
    * updateCube: 회전 후 큐브 업데이트

--- src/PlaneCube.java
+++ src/PlaneCube.java
@@ -1,63 +1,34 @@
-import com.sun.xml.internal.stream.Entity;
 
 import java.util.Scanner;
 
 public class PlaneCube {
-    public PlaneCube(String cube[][]){
-        printCube(cube);
-        run(cube);
+    private final String[][] cube;
+
+    public PlaneCube(String[][] cube){
+        this.cube = cube;
+        printCube();
     }
 
-    private  String inputLine(){
-        //라인 입력받기
-        System.out.print("cube> ");
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-
-        return line;
-    }
-
-    private  void run(String cube[][]){
+    public void run(){
         while(true){
             //실행
             String line = inputLine();
             switch (line){
                 case "U": case "u":
-                    StringBuilder word = new StringBuilder();
-                    for(int i = 0; i < cube[0].length; i++){
-                        word.append(cube[0][i]);
-
-                    }
-                    String word2 = word.toString();
-                    new PushingWord(word2,1,"L");
-                    printCube(cube);
-
+                    TurnLeft("U");
+                    printCube();
                     break;
                 case "R": case "r":
-                    StringBuilder wordR = new StringBuilder();
-                    for(int i = 0; i < cube.length; i++){
-                        wordR.append(cube[i][2]);
-
-                    }
-                    String wordR2 = wordR.toString();
-                    new PushingWord(wordR2,1,"L");
+                    TurnLeft("R");
+                    printCube();
                     break;
                 case "L": case "l":
-                    StringBuilder wordL = new StringBuilder();
-                    for(int i = 0; i < cube.length; i++){
-                        wordL.append(cube[i][0]);
-
-                    }
-                    String wordL2= wordL.toString();
-                    new PushingWord(wordL2,1,"R");
+                    TurnRight("L");
+                    printCube();
                     break;
                 case "B": case "b":
-                    StringBuilder wordB = new StringBuilder();
-                    for(int i = 0; i < cube[2].length; i++){
-                        wordB.append(cube[2][i]);
-                    }
-                    String wordB2 = wordB.toString();
-                    new PushingWord(wordB2,1,"L");
+                    TurnRight("B");
+                    printCube();
                     break;
                 case "Q": case "q":
                     System.out.println("Bye~~!");
@@ -66,19 +37,103 @@ public class PlaneCube {
             }
         }
     }
-    private  void printCube(String cube[][]){
-        //배열 출력
-        for(int i = 0; i < cube.length; i++){
-            for(int j =0; j < cube[i].length; j++){
-                System.out.print(cube[i][j] + " ");
-            }
-            System.out.println();
+
+    //라인(U,R,L,B) 입력받기
+    private  String inputLine(){
+        System.out.print("cube> ");
+        Scanner sc = new Scanner(System.in);
+
+        return sc.nextLine();
+    }
+
+    //왼쪽으로 돌리기
+    private void TurnLeft(String line) {
+        //라인의 배열 -> 문자열로 합치기: ["R","W,"R"] ->"RWR"
+        String word = ArrayToString(line);
+
+        //문자열 회전
+        PushingWord pushingWord = new PushingWord(word,1,"L");
+        String result = pushingWord.printWord();
+
+        System.out.println(line);
+        updateCube(result,line);
+    }
+
+    //오른쪽으로 돌리기
+    private void TurnRight(String line){
+        String word = ArrayToString(line);
+
+        //문자열 회전
+        PushingWord pushingWord = new PushingWord(word,1,"R");
+        String result = pushingWord.printWord();
+
+        System.out.println(line);
+        updateCube(result,line);
+    }
+
+    //라인의 배열 -> 문자열로 합치기: ["R","W,"R"] ->"RWR"
+    private String ArrayToString(String line) {
+        StringBuilder word = new StringBuilder();
+
+        switch (line) {
+            case "U":
+                for (int i = 0; i < cube[0].length; i++) {
+                    word.append(cube[0][i]);
+                }
+                break;
+            case "R":
+                for (String[] strings : cube) {
+                    word.append(strings[2]);
+                }
+                break;
+            case "B":
+                for (int i = 0; i < cube[2].length; i++) {
+                    word.append(cube[2][i]);
+                }
+                break;
+            case "L":
+                for (String[] strings : cube) {
+                    word.append(strings[0]);
+                }
+                break;
+        }
+
+        return word.toString();
+    }
+
+
+    //회전 후, 큐브 업데이트
+    private void updateCube(String str,String line) {
+        switch (line){
+            case "U": //Up
+                cube[0] = str.split(""); // ["R","W","R"]
+                break;
+            case "R": //Right
+                for(int i = 0; i < cube.length; i++){
+                    cube[i][2] = str.split("")[i];
+                }
+                break;
+            case "B": //Back
+                cube[2] = str.split("");
+                break;
+            case "L": //Left
+                for(int i = 0; i < cube.length; i++){
+                    cube[i][0] = str.split("")[i];
+                }
+                break;
         }
 
     }
-    public static void main(String[] args){
-        String cube[][] = {{"R","R","W"},{"G","C","W"},{"G","B","B"}};
 
-        new PlaneCube(cube);
+    //큐브 출력
+    private  void printCube(){
+        for (String[] strings : cube) {
+            for (String string : strings) {
+                System.out.print(string + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
+
 }
