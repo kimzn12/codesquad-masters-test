86243e4b2f01155c62b32fceaeface4477495fdd
kimzn12 <kimzn12@naver.com>
2020-12-12 21:58:54 +0900

    [add] 큐브 블록

---
+++ step-3/src/Block.java
@@ -0,0 +1,15 @@
+package src;
+
+import javax.swing.*;
+
+public class Block {
+    private String color;
+
+    public Block(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+}
