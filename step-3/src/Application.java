f5b9e37e9603c8b53fcb075671525e02cf673f2c
kimzn12 <kimzn12@naver.com>
2020-12-14 04:22:23 +0900

    add: Run the Rubik's Cube

--- step-3/src/Application.java
+++ step-3/src/Application.java
@@ -1,7 +1,11 @@
 package src;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args){
+        RubiksCube rubiksCube = new RubiksCube();
+        rubiksCube.run();
 
     }
 }
